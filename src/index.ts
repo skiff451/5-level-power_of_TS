a6bf711104fde05e678f3f8f80e439cca7de56e1
Serghey <samsomskiff451@gmail.com>
2021-02-03 17:19:38 +0200

    fix: return undefined bug

--- src/index.ts
+++ src/index.ts
@@ -24,18 +24,19 @@ function summ(a: BigObject): number {
   if (Object.keys(a).length === 0) {
     return -1;
   }
-  const x: (number | undefined)[] = Object.keys(a).map((k) => {
+  const x: number[] = Object.keys(a).map((k) => {
     const elem = a[k];
     if (elem === undefined) return 2021;
     if (typeof elem.cvalue === "string") return +elem.cvalue || 2021;
     if (typeof (elem.cvalue as BigObject) === "object")
       return summ(elem.cvalue as BigObject);
     if (typeof elem.cvalue === "number") return elem.cvalue;
+    return 0;
   });
 
   let sum = 0;
   for (let i = 0; i < x.length; i++) {
-    sum += x[i] || 0;
+    sum += x[i];
   }
   return sum;
 }
